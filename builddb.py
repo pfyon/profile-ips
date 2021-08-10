3a92503ad345a42ae1291effe8e13331ee0fdeed
Peter <peter.fyon@gmail.com>
2021-08-10 00:37:34 -0400

    Write the csv file. Store columns in a variable so we can write our csv header using them. Fix spelling of dst_ip to dest_ip.

--- builddb.py
+++ builddb.py
@@ -2,10 +2,13 @@
 
 import argparse
 import collections
+import csv
 import datetime
 import gzip
+import json
 import logging
 import netaddr
+import os
 import sys
 
 class Processor(object):
@@ -13,7 +16,8 @@ class Processor(object):
 		self._path = path
 		self._dest = dest
 		self._ips = important_ips
-		self.Record = collections.namedtuple('Record', ['ip', 'ts_start', 'ts_end', 'src_port', 'dst_port', 'pkts_toclient', 'pkts_toserver', 'bytes_toclient', 'bytes_toserver', 'proto', 'app'])
+		self._columns = ['ip', 'ts_start', 'ts_end', 'src_port', 'dst_port', 'pkts_toclient', 'pkts_toserver', 'bytes_toclient', 'bytes_toserver', 'proto', 'app']
+		self.Record = collections.namedtuple('Record', self._columns)
 
 		if os.path.exists(self._dest):
 			if not os.path.isfile(self._dest):
@@ -23,17 +27,22 @@ class Processor(object):
 
 	def process(self):
 		if os.path.exists(self._path):
-			if not os.path.isfile(self._path):
-				#a directory
-				for filename in os.listdir(self._path):
-					 records = self._processFile(os.path.join(self._path, filename))
-			else:
-				#a file
-				records = self._processFile(self._path)
+			with open(self._dest, 'w', newline='') as csvfile:
+				writer = csv.writer(csvfile)
+				writer.writerow(self._columns)
+
+				if not os.path.isfile(self._path):
+					#a directory
+					for filename in os.listdir(self._path):
+						writer.writerows(self._processFile(os.path.join(self._path, filename)))
+				else:
+					#a file
+					writer.writerows(self._processFile(self._path))
 		else:
 			raise FileNotFoundError(f"'{self._path}' doesn't exist")
 
 	def _processFile(self, path):
+		records = []
 		try:
 			f = gzip.GzipFile(path, mode='r')
 			f.peek(10) #Test to see if it's a real gzip file
@@ -48,62 +57,62 @@ class Processor(object):
 				msg = json.loads(line)
 				if msg.get('event_type', None) == 'flow':
 					ip = None
-					if 'src_ip' in msg and netaddr.IPAddress(msg.get('src_ip')) in self.ips:
+					if 'src_ip' in msg and netaddr.IPAddress(msg.get('src_ip')) in self._ips:
 						ip = netaddr.IPAddress(msg.get('src_ip'))
-					elif 'dst_ip' in msg and netaddr.IPAddress(msg.get('dst_ip')) in self.ips:
-						ip = netaddr.IPAddress(msg.get('dst_ip'))
+					elif 'dest_ip' in msg and netaddr.IPAddress(msg.get('dest_ip')) in self._ips:
+						ip = netaddr.IPAddress(msg.get('dest_ip'))
 
-					ts_start = None
-					ts_end = None
-					src_port = None
-					dst_port = None
-					pkts_toclient = None
-					pkts_toserver = None
-					bytes_toclient = None
-					bytes_toserver = None
-					proto = None
-					app = None
-
-					src_port = int(msg['src_port']) if 'src_port' in msg else None
-					dst_port = int(msg['dst_port']) if 'dst_port' in msg else None
-
-					proto = msg.get('proto', None)
-
-					if proto == 'TCP':
-						proto = 6
-					elif proto == 'UDP':
-						proto = 17
-					elif proto == 'ICMP':
-						proto = 1
-					elif proto == 'IPv6-ICMP': 
-						proto = 58
-					elif proto == 'SCTP':
-						proto = 132
-					elif proto is None:
-						pass
-					else:
-						self.log.warn(f"Unsupported protocol '{proto}'")
-						self.log.warn(msg)
-
-					app = msg.get('app_proto', None)
-					
-					if 'flow' in msg:
-						if 'start' not in msg['flow'] or end not in msg['flow']:
-							self.log.debug("Skipping this record - it's missing a start or end in flow")
-							continue
-
-						ts_start = datetime.datetime.strptime(msg['flow']['start'], '%Y-%m-%dT%H:%M:%S.%f%z').timestamp()
-						ts_end   = datetime.datetime.strptime(msg['flow']['end'],   '%Y-%m-%dT%H:%M:%S.%f%z').timestamp()
-
-						pkts_toclient = msg['flow'].get('pkts_toclient', None)	
-						pkts_toserver = msg['flow'].get('pkts_toserver', None)	
-
-						bytes_toclient = msg['flow'].get('bytes_toclient', None)
-						bytes_toserver = msg['flow'].get('bytes_toserver', None)
-						
 					if ip is not None:
+						ts_start = None
+						ts_end = None
+						src_port = None
+						dst_port = None
+						pkts_toclient = None
+						pkts_toserver = None
+						bytes_toclient = None
+						bytes_toserver = None
+						proto = None
+						app = None
+	
+						src_port = int(msg['src_port']) if 'src_port' in msg else None
+						dst_port = int(msg['dst_port']) if 'dst_port' in msg else None
+	
+						proto = msg.get('proto', None)
+	
+						if proto == 'TCP':
+							proto = 6
+						elif proto == 'UDP':
+							proto = 17
+						elif proto == 'ICMP':
+							proto = 1
+						elif proto == 'IPv6-ICMP': 
+							proto = 58
+						elif proto == 'SCTP':
+							proto = 132
+						elif proto is None:
+							pass
+						else:
+							log.warn(f"Unsupported protocol '{proto}'")
+							log.warn(msg)
+	
+						app = msg.get('app_proto', None)
+						
+						if 'flow' in msg:
+							if 'start' not in msg['flow'] or 'end' not in msg['flow']:
+								log.debug("Skipping this record - it's missing a start or end in flow")
+								continue
+	
+							ts_start = datetime.datetime.strptime(msg['flow']['start'], '%Y-%m-%dT%H:%M:%S.%f%z').timestamp()
+							ts_end   = datetime.datetime.strptime(msg['flow']['end'],   '%Y-%m-%dT%H:%M:%S.%f%z').timestamp()
+	
+							pkts_toclient = msg['flow'].get('pkts_toclient', None)	
+							pkts_toserver = msg['flow'].get('pkts_toserver', None)	
+	
+							bytes_toclient = msg['flow'].get('bytes_toclient', None)
+							bytes_toserver = msg['flow'].get('bytes_toserver', None)
+							
 						record = self.Record(
-							ip = ip.value,
+							ip.value,
 							ts_start,
 							ts_end,
 							src_port,
@@ -115,10 +124,13 @@ class Processor(object):
 							proto,
 							app
 						)
-					#TODO: put record somewhere
-					#TODO put this in git
+	
+						records.append(record)
 		finally:
 			f.close()
+
+		log.debug(f"Found {len(records)} flow records")
+		return records
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Parse one or more eve.json files and extract flow into a csv file suitable for profile.py")
